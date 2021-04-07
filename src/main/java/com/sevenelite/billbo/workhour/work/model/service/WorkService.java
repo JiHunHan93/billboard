6f06bf1f447e34ae8c14af0e9f45d02a8e6b22c9
JiHunHan93 <74941254+JiHunHan93@users.noreply.github.com>
2021-04-07 19:09:20 +0900

    지훈 긴급복구

--- src/main/java/com/sevenelite/billbo/workhour/work/model/service/WorkService.java
+++ src/main/java/com/sevenelite/billbo/workhour/work/model/service/WorkService.java
@@ -1,11 +1,11 @@
-package com.sevenelite.billbo.workhour.work.model.service;
-
-import java.util.List;
-
-import com.sevenelite.billbo.workhour.work.model.dto.WorkDTO;
-
-public interface WorkService {
-	//근태조회
-	List<WorkDTO> selectWorkList();
-
-}
+//package com.sevenelite.billbo.workhour.work.model.service;
+//
+//import java.util.List;
+//
+//import com.sevenelite.billbo.workhour.work.model.dto.WorkDTO;
+//
+//public interface WorkService {
+//	//근태조회
+//	List<WorkDTO> selectWorkList();
+//
+//}
