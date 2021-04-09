75ee2da04c555c2bfb4e173bbc917ddfe4ab2f60
Songnamgyu <namgyu1020@naver.com>
2021-04-09 18:16:50 +0900

    20210409 금 송남규 게시판 삭제기능구현 완료 , 수정기능구현중

--- src/main/java/com/sevenelite/billbo/board/model/service/BoardService.java
+++ src/main/java/com/sevenelite/billbo/board/model/service/BoardService.java
@@ -8,9 +8,7 @@ import com.sevenelite.billbo.board.model.dto.BoardDTO;
 public interface BoardService {
 
 	List<BoardDTO> selectBoard();
-	/*
-	 * int insertBoard(BoardDTO boardDTO);
-	 */
+
 
 	boolean registBoard(BoardDTO board);
 
@@ -18,6 +16,7 @@ public interface BoardService {
 
 	int updateCount(int no);
 
-	/* BoardDTO detailBoard(int no); */
+	int deleteBoard(int no);
 
+	
 }
