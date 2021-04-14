ca74c61bf8ddf53db7f520689fc5acbcff37f5e3
epyi12345 <74941279+epyi12345@users.noreply.github.com>
2021-04-14 11:18:34 +0900

    이유진_0414수_1 아침커밋

--- src/main/java/com/sevenelite/billbo/board/model/service/BoardService.java
+++ src/main/java/com/sevenelite/billbo/board/model/service/BoardService.java
@@ -7,21 +7,27 @@ import com.sevenelite.billbo.board.model.dto.BoardDTO;
 
 public interface BoardService {
 
+//  자유 게시판
 	List<BoardDTO> selectBoard();
-
-
-	boolean registBoard(BoardDTO board);
 
 	List<BoardDTO> detailBoard(int no);
 
-	int updateCount(int no);
-
-	int deleteBoard(int no);
-
-	boolean updateBoard(BoardDTO boardDTO);
-
+//  인사 게시판
 	List<BoardDTO> selectEmpBoard();
 
 	List<BoardDTO> detailEmpBoard(int no);
+
+//  공지 게시판	
+	List<BoardDTO> selectNoticeBoard();
+
+	List<BoardDTO> detailNoticeBoard(int no);
 	
+//  공용
+	int updateCount(int no);
+	
+	int deleteBoard(int no);
+	
+	boolean registBoard(BoardDTO board);
+	
+	boolean updateBoard(BoardDTO boardDTO);
 }
