e30ac5f62812839120b88dcae7a6c5270f8a75a2
epyi12345 <74941279+epyi12345@users.noreply.github.com>
2021-04-15 12:05:30 +0900

    이유진_0415목_1 아침커밋

--- src/main/java/com/sevenelite/billbo/calender/controller/CalenderController2.java
+++ src/main/java/com/sevenelite/billbo/calender/controller/CalenderController2.java
@@ -1,8 +1,13 @@
 package com.sevenelite.billbo.calender.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sevenelite.billbo.calender.model.dto.CalDTO;
 
 @Controller
 @RequestMapping("/calender2/*")
@@ -11,6 +16,10 @@ public class CalenderController2 {
 	@GetMapping("main")
 	public String CalenderMain() {
 		
+		Map<String, CalDTO> javaMap = new HashMap<String, CalDTO>();
+		
+//		javaMap.put("evt1", new CalDTO("db이벤트1", "2021-04-04", "2021-04-06", "false") );
+//		javaMap.put("evt2", new CalDTO("db이벤트2", "2021-04-23", "2021-04-26", "false") ); 
 		return "Calender/calender2";
 	}
 	
