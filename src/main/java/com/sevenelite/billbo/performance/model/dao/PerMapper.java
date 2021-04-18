a59afe5453c2c1ffb4d4337ab1b5c0105839cf56
JiHunHan93 <74941254+JiHunHan93@users.noreply.github.com>
2021-04-18 21:50:08 +0900

    (PM09일)한지훈_0418일_03 일요일 서버 오류 수정
    주말 머지 서버 터진거 복구

--- src/main/java/com/sevenelite/billbo/performance/model/dao/PerMapper.java
+++ src/main/java/com/sevenelite/billbo/performance/model/dao/PerMapper.java
@@ -1,12 +1,12 @@
-package com.sevenelite.billbo.performance.model.dao;
-
-import java.util.List;
-
-import com.sevenelite.billbo.performance.model.dto.PerformanceListDTO;
-
-
-public interface PerMapper {
-
-	List<PerformanceListDTO> selectPer();
-
-}
+//package com.sevenelite.billbo.performance.model.dao;
+//
+//import java.util.List;
+//
+//import com.sevenelite.billbo.performance.model.dto.PerformanceListDTO;
+//
+//
+//public interface PerMapper {
+//
+//	List<PerformanceListDTO> selectPer();
+//
+//}
--- src/main/java/com/sevenelite/billbo/performance/model/service/PerformanceServiceImpl.java
+++ src/main/java/com/sevenelite/billbo/performance/model/service/PerformanceServiceImpl.java
@@ -1,27 +1,27 @@
-package com.sevenelite.billbo.performance.model.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.sevenelite.billbo.performance.model.dao.PerMapper;
-import com.sevenelite.billbo.performance.model.dto.PerformanceListDTO;
-
-@Service("perService")
-public class PerformanceServiceImpl implements PerformanceService {
-	
-	private PerMapper perMapper;
-	
-	@Autowired
-	public PerformanceServiceImpl(PerMapper perMapper) {
-		this.perMapper = perMapper;
-	}
-	
-	@Override
-	public List<PerformanceListDTO> selectPer() {
-		
-		return perMapper.selectPer();
-	}
-
-}
+//package com.sevenelite.billbo.performance.model.service;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.sevenelite.billbo.performance.model.dao.PerMapper;
+//import com.sevenelite.billbo.performance.model.dto.PerformanceListDTO;
+//
+//@Service("perService")
+//public class PerformanceServiceImpl implements PerformanceService {
+//	
+//	private PerMapper perMapper;
+//	
+//	@Autowired
+//	public PerformanceServiceImpl(PerMapper perMapper) {
+//		this.perMapper = perMapper;
+//	}
+//	
+//	@Override
+//	public List<PerformanceListDTO> selectPer() {
+//		
+//		return perMapper.selectPer();
+//	}
+//
+//}
