3a755d2fc97ad2efbd19c09714f4db47c69ee1fc
choijihwan <dawn3118@naver.com>
2021-04-18 21:25:59 +0900

    choijihwan_0418_상벌관리 완료 및 인사평가 뷰페이지 및 셀렉트 구현

--- src/main/java/com/sevenelite/billbo/performance/model/dao/PerMapper.java
+++ src/main/java/com/sevenelite/billbo/performance/model/dao/PerMapper.java
@@ -1,10 +1,11 @@
-package com.sevenelite.billbo.performance.model.service;
+package com.sevenelite.billbo.performance.model.dao;
 
 import java.util.List;
 
 import com.sevenelite.billbo.performance.model.dto.PerformanceListDTO;
 
-public interface PerformanceService {
+
+public interface PerMapper {
 
 	List<PerformanceListDTO> selectPer();
 
