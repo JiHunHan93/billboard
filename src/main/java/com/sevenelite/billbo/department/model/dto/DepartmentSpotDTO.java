8131602ee51798e60b2fb571f5a0142c27c13df5
minseo <rab_boy@naver.com>
2021-04-19 12:25:49 +0900

    heuisangchoi_0419_02

--- src/main/java/com/sevenelite/billbo/department/model/dto/DepartmentSpotDTO.java
+++ src/main/java/com/sevenelite/billbo/department/model/dto/DepartmentSpotDTO.java
@@ -4,60 +4,11 @@ import java.sql.Date;
 
 public class DepartmentSpotDTO {
 
-	/* Customizing : ROWNUM + BB_SPOT + BB_MODIFY_SPOT */
-	/* 전자결재를 위한 로그인 회원의 직위 정보 */
-	
 	private int spotRnum;					//순번
 	private String spotCode;				//직위코드
 	private String spotName;				//직위명
+	private String memberName;              //이름 
 	private java.sql.Date modifySpotDate;	//변경날짜
 	
-	public DepartmentSpotDTO() {}
-
-	public DepartmentSpotDTO(int spotRnum, String spotCode, String spotName, Date modifySpotDate) {
-		super();
-		this.spotRnum = spotRnum;
-		this.spotCode = spotCode;
-		this.spotName = spotName;
-		this.modifySpotDate = modifySpotDate;
-	}
-
-	public int getSpotRnum() {
-		return spotRnum;
-	}
-
-	public void setSpotRnum(int spotRnum) {
-		this.spotRnum = spotRnum;
-	}
-
-	public String getSpotCode() {
-		return spotCode;
-	}
-
-	public void setSpotCode(String spotCode) {
-		this.spotCode = spotCode;
-	}
-
-	public String getSpotName() {
-		return spotName;
-	}
-
-	public void setSpotName(String spotName) {
-		this.spotName = spotName;
-	}
-
-	public java.sql.Date getModifySpotDate() {
-		return modifySpotDate;
-	}
-
-	public void setModifySpotDate(java.sql.Date modifySpotDate) {
-		this.modifySpotDate = modifySpotDate;
-	}
-
-	@Override
-	public String toString() {
-		return "ApproSpotDTO [spotRnum=" + spotRnum + ", spotCode=" + spotCode + ", spotName=" + spotName
-				+ ", modifySpotDate=" + modifySpotDate + "]";
-	}
 	
 }
